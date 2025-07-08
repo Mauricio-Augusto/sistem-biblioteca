2b2a79fad8abaf22b69da1a2b205e9089a141b8d
MAURICIO AUGUSTO MIRANDA <mauricioaugusto1503@gmail.com>
2025-07-08 18:57:58 -0400

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO Autor VALUES (1, 'Machado de Assis');
+INSERT INTO Livro VALUES (1, 'Dom Casmurro', 1899, 1);
+INSERT INTO Usuario VALUES (1, 'João Silva', 'joao@email.com');
+INSERT INTO Emprestimo VALUES (1, 1, 1, '2025-07-01', NULL);
